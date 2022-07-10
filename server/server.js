16d64855c7aca1ef427e1a68641606ec3e3b0111
Chris Abiva <chrisabiva@hotmail.com>
2022-07-10 12:42:52 -0700

    Stating server

--- server/server.js
+++ server/server.js
@@ -4,5 +4,29 @@ const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleWare } = require('./utils/auth');
+const db = require('./config/connection');
 
-const 
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleWare
+});
+
+server.applyMiddleware({ extended: true });
+app.use(express.json());
+
+app.use('./images/', express.static(path.join(__dirname, '..client/images')));
+
+app.use(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build/')));
+};
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+  app.listen(PORT, '192.168.1.104');
+});
