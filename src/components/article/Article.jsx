309dab13cf8eeb12e1785b31c4b857bc189c9427
Chris Abivz <chrisabiva@hotmail.com>
2022-10-27 23:30:40 -0700

    updating the images on for projects

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import './article.css'
+import './article.css';
 
-const Article = ({imgUrl, date, title, github, about}) =>  {
+const Article = ({imgUrl, date, title, github, about, page}) =>  {
   return (
     <div className='gpt3__blog-container_article'>
       <div className='gpt__blog-container_article-image'>
-        <img src={imgUrl} alt="Projects"/>
+        <img className='photo' src={imgUrl} alt="Projects"/>
       </div>
       <div className='gpt3__blog-container_article-content'>
         <div>
           <p>{date}</p>
           <h3>{title}</h3>
           <p>{about}</p>
-          <p><a href={github} target="_blank" rel="noreferrer">Read More</a></p>
+          <p><a href={github} target="_blank" rel="noreferrer">Github Link</a></p>
+          <p><a href={page} target="_blank" rel="noreferrer">Live Page</a></p>
         </div>
       </div>
     </div>
